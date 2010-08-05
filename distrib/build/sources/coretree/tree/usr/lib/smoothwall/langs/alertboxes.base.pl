b510d6aa0fca58fdcd8a5c86e739b2297d2e41c3
NPMurphy <neal.p.murphy@alum.wpi.edu>
2010-08-05 17:08:21 -0400

    Swap *base.pk with *en.pl so translations will work.

--- distrib/build/sources/coretree/tree/usr/lib/smoothwall/langs/alertboxes.base.pl
+++ distrib/build/sources/coretree/tree/usr/lib/smoothwall/langs/alertboxes.base.pl
@@ -4,61 +4,8 @@
 #
 # (c) The SmoothWall Team
 
-%baseabouttext = (
+%abouttext = (
 
-'index.cgi' => '
-Welcome to <strong>SmoothWall Express</strong> ' . $version . '<br>
-This is your gateway to configuring and administering your SmoothWall
-firewall.  Further information on your SmoothWall Express is available from <a href=\'http://www.smoothwall.org/\'
- title=\'www.smoothwall.org - external link\'>our website</a>.
-',
-
-'credits.cgi' 		=> 'SmoothWall Express' . $tr{'version'} . ' ' . $version . '-' . $revision . ' ' . $webuirevision,
-'status.cgi' 		=> 'Active service status of your Smoothie.',
-'advstatus.cgi' 	=> 'Pertinent information about your Smoothie, current configuration and resource usage.',
-'graphs.cgi' 		=> 'Statistical graphical and numeric data based upon traffic usage across your SmoothWall\'s network interfaces.',
-'proxy.cgi' 		=> 'Configure and enable your SmoothWall\'s integrated caching web proxy service.',
-'dhcp.cgi' 		=> 'Configure and enable your SmoothWall\'s DHCP service, to automatically allocate LAN IP addresses to your network clients.',
-'ddns.cgi' 		=> 'Especially suited when your ISP assigs you a different IP address every time you connect, you can configure your SmoothWall to manage and update your dynamic DNS names from several popular services.',
-'ids.cgi' 		=> 'Enable the Snort IDS service to detect potential security breach attempts from outside your network.  Note that Snort <strong>does not</strong> prevent these attempts &mdash; your port forwarding and access rules are used to allow and deny inbound access from the outside.<br>Snort is no longer shipped with any rules. In order to fetch rules you need to visit <a href="http://www.snort.org" target="_new">www.snort.org</a> and register for an Oink code',
-'remote.cgi' 		=> 'Enable Secure Shell access to your SmoothWall, and restrict access based upon referral URL to ignore external links to your SmoothWall.',
-'portfw.cgi' 		=> 'Forward ports from your external IP address to ports on machines inside your local networks.',
-'xtaccess.cgi' 		=> 'Allow access to admin services running on the SmoothWall to external hosts.',
-'dmzholes.cgi' 		=> 'Enable access from a host on your ORANGE or PURPLE networks to a port on a host on your GREEN network.',
-'pppsetup.cgi' 		=> 'Configure username, password and other details for up to five PPP, PPPoA or PPPoE connections.',
-'vpnmain.cgi' 		=> 'Control and manage your VPN connections.',
-'vpnconfig.dat' 	=> 'Create connections to other SmoothWalls or IPSec-compliant hosts which have static IP addresses.',
-'log.dat' 		=> 'Check activity logs for services operating on your SmoothWall, such as DHCP, IPSec, updates and core kernel activity',
-'proxylog.dat' 		=> 'Check logs for the web proxy service.',
-'firewalllog.dat'	=> 'Check logs for attempted access to your network from outside hosts.  Connections listed here <strong>have</strong> been blocked.',
-'ids.dat' 		=> 'Check logs for potentially malicious attempted access to your network from outside hosts.  Connections listed here <strong>have not necessarily</strong> been blocked &mdash; use the Firewall Log Viewer to confirm blocked access.',
-'ipinfo.cgi' 		=> 'Perform a \'whois\' lookup on an ip address or domain name.',
-'iptools.cgi' 		=> 'Perform \'ping\' and \'traceroute\' network diagnostics.',
-'shell.cgi' 		=> 'Connect to your SmoothWall using a Java SSH applet (requires SSH to be <a href="/cgi-bin/remote.cgi">enabled</a>).',
-'updates.cgi' 		=> 'See the latest updates and fixes available for your SmoothWall, and an installation history of updates previously applied.',
-'modem.cgi' 		=> 'Apply specific AT string settings for your PSTN modem or ISDN TA.',
-'alcateladslfw.cgi' 	=> 'Upload firmware to enable use of an Alcatel/Thomson Speedtouch Home USB ADSL modem, nicknamed the \'frog\' or \'stingray\'.  <a href="http://smoothwall.org/get/" target="_breakFromUI">Download the \'Speedtouch USB Firmware\' tarball</a>, unpack it, and upload the mgmt.o file using this form.',
-'changepw.cgi' 		=> 'Change passwords for the \'admin\' and \'dial\' management interface users.  This does not affect access by SSH.',
-'shutdown.cgi' 		=> 'Shutdown or restart your SmoothWall &mdash; restarts are sometimes mandated by update installation.',
-'time.cgi' 		=> 'Change timezone, manually set the time and date, configure time syncronisation and enable the time server.',
-'advnet.cgi' 		=> 'Configure ICMP settings and other advanced features.',
-'outbound.cgi'		=> 'Configure interfaces to have outbound traffic blocked, except on specific rules.',
-'ipblock.cgi' 		=> 'Add blocking rules to prevent access from specified IP addresses or networks.',
-'backup.img' 		=> 'Use this page to create a backup floppy disk or floppy disk image file.',
-'im.cgi'		=> 'Configure the IM logging proxy.',
-'outgoing.cgi' 		=> 'Add rules to control local machine\'s access to external services.',
-'traffic.cgi'		=> 'Set the best speed which your network is capable of achieving. The headroom should be increased if traffic shaping is having no effect.',
-'sipproxy.cgi'		=> 'Configure the SIP proxy service. It can be used to transparently or non-transparently proxy SIP calls to and from the GREEN network.',
-'interfaces.cgi'	=> 'Configure the network interface IP addresses, as well as DNS and gateway settings.',
-'hosts.cgi'		=> 'Add static DNS entries to SmoothWall\'s inbuilt DNS server.',
-'timedaccess.cgi'	=> 'Configure timed access rules to prevent or allow internal machines network access at certain times of the day.',
-'p3scan.cgi'		=> 'Configure the POP3 anti-virus proxy. This proxy will remove viruses from emails that are retrieved through using the POP3 protocol.',
-'pop3log.dat'		=> 'Check log for the POP3 Anti-Virus service.',
-'imviewer.cgi'		=> 'View logged IM conversations in realtime.',
-'preferences.cgi'	=> 'Configure the SmoothWall Express User Interface.',
-'bandwidthbars.cgi'	=> 'Shows realtime network bandwidth usage bars.',
-'trafficmonitor.cgi'	=> 'Shows realtime network bandwidth usage graphs.',
-'register.cgi'		=> 'Shows information about your SmoothWall Express system.',
+%baseabouttext
 
 );
-
--- distrib/build/sources/coretree/tree/usr/lib/smoothwall/langs/glossary.base.pl
+++ distrib/build/sources/coretree/tree/usr/lib/smoothwall/langs/glossary.base.pl
@@ -4,52 +4,8 @@
 #
 # (c) The SmoothWall Team
 
-%baseglossary = (
-	'De-Militarized Zone'  => "<strong>De-Militarized Zone</strong> A common term for a logically isolated network used to house machines which expose services to the internet (such as HTTP or mail servers) and are therefor isolated from the locally protected network.",
-	'DMZ'  => "<strong>De-Militarized Zone</strong> A common term for a logically isolated network used to house machines which expose services to the internet (such as HTTP or mail servers) and are therefor isolated from the locally protected network.",
-	'ORANGE' => "<strong>ORANGE</strong> The name usually given for the interface on a SmoothWall system which provides the De-Militarized Zone.",
-	'GREEN' => "<strong>GREEN</strong> The local network usually reserved for desktop machines and servers. It can access all the other internal networks.",
-	'PURPLE' => "<strong>PURPLE</strong> An optional additional network intended to be used for wireless laptops and other devices. Machines on PURPLE can access ORANGE but not GREEN.",
-	'PPP' => "<strong>Point to Point Protocol</strong> A protocol used for connecting to an ISP over a serial-like conection, such a analogue modem or ADSL.",
-	'ADSL' => "<strong>Asymmetric Digital Subscriber Line</strong> A mechanism for connecting to the Internet at broadband speeds over conventional copper phone lines.",
-	'ISDN' => "<strong>Integrated Standards Digital Network</strong> A moderate-speed (64Kbit/sec usually) method for connecting to the Internet.",
-	'Port' => "<strong>Port</strong> Each Internet service, such HTTP or SMTP, is assigned a unique number which identifies the service. Also a general name for a connector on a PC, such a serial port.",
-	'IP' => "<strong>Internet Protocol (address)</strong> Each host on an IP network is assigned a numerical address in the form 1.2.3.4. These addresses can either be external (on the wider Internet) or local (on the local network).",
-	'TCP' => "<strong>Transmission Control Protocol</strong> A network protocol that guarantees reliable and in-order delivery of data between Internet hosts",
-	'UDP' => "<strong>User Datagram Protocol</strong> A network protocol that provides a connection-less data exchange. Delivery of data is not guaranteed.",
-	'MSN' => "<strong>Microsoft Messenger</strong> A program for talking to your mates and sending them pictures, videos and the like.",
-	'IRC' => "<strong>Internet Relay Chat</strong> A realtime communications network and protocol developed in 1988. The first widespread Internet chat system.",
-	'VOIP' => "<strong>Voice over Internet Protocol</strong> A catch-all term for any protocol or program that provides voice and/or data calls over the public Internet. VOIP calls are effectivly free, but can suffer from quality problems.",
-	'SYN cookie' => "<strong>SYN cookie</strong> A mechanism for avoiding Denial of Service attacks.",
-	'UPnP' => "<strong>Universal Plug n Play</strong> A collection of mechanisms and protocols for automating network setup, and other tasks. In SmoothWall UPnP support is used to allow programs such as MSN to work better when connecting through a NATed Internet connection.",
-	'Traceroute' => "<strong>Traceroute</strong> A program that can be used to determine the path a packet will take on its way to an Inernet host.",
-	'Ping' => "<strong>Ping</strong> A program for determining whether a machine is up and running, and also for measuring the time it takes to reach it.",
-	'SSH' => "<strong>Secure Shell</strong> A program that can be used to gain command-line access to another machine, with the ability to encrypt the session. It also has strong authentication.",
-	'HTTP' => "<strong>Hyper-Text Transfer Protocol</strong> The protcol used to transfer webpages and simular content between webservers and browsers.",
-	'HTTPS' => "<strong>Hyper-Text Transfer Protcol (Secure)</strong> An extention to HTTP to provide security in the form of encryption and identification.",
-	'FTP' => "<strong>File Transfer Protocol</strong> An antiquated protocol for use in file transfer between Internet hosts. It provides little in the way of authentication and encryption.",
-	'POP3' => "<strong>Post Office Protocol 3</strong> A simple protocol used in the downloading of Emails between a server and a client. Extensions exist for secure authentication and encryption.",
-	'NAT' => "<strong>Network Address Translation</strong> Any form of IP address munging, but in a SmoothWall context it is the mechanism by which an internal host on GREEN or any other interface accesses the Internet with only a single real external IP address.",
-	'NTP' => "<strong>Network Time Protocol</strong> A protocol for telling machines, accurately, what the time is on a network. It is used both on the Internet and on LANs.",
-	'VNC' => "<strong>Virtual Network Computing</strong> A set of programs, that allows the remote access of another computer\\'s desktop. Available for all major Operating Systems.",
-	'SSL' => "<strong>Secure Sockets Layer</strong> A layering protocol that can be used to add encryption and authentication to a lower-level protocol, such as POP3 or HTTP.",
-	'ClamAV' => "<strong>Clam AntiVirus</strong> An open source (GPL) anti-virus toolkit, designed especially for e-mail scanning on mail gateways.",
-	'DNS' => "<strong>Domain Name System</strong> A protocol and service for the resolution of human-understandable names, such as www.smoothwall.org, to IP addresses, and vica-versa.",
-	'DHCP' => "<strong>Dynamic Host Configuration Protocol</strong> A protocol and service for automatically configuring machines on a network. DHCP typically furnishes hosts with an IP address, DNS settings, and gateway information.",
-	'PCI' => "<strong>Peripheral Component Interconnect</strong> A standard for attaching peripheral devices inside of a PC. Replaces the older ISA and VESA standards.",
-	'Linux' => "<strong>Linux</strong> An Open Source Operating System originally written by Linus Torvalds.",
-	'ITSP' => "<strong>Internet Telephony Service Provider</strong> An ITSP offers an Internet data service for making telephone calls using VoIP (Voice over IP) technology.",
-	'MAC' => "<strong>Media Access Control (address) </strong> The hardware address of a network device, typically a NIC, which is hardcoded into the device\\'s firmware.",
-	'LAN' => "<strong>Local Area Network</strong> A small computer network, usually confined to a single building.",
-	'URL' => "<strong>Uniform Resource Locator</strong> A sequence of characters representing the location of a resource on the Internet, such a webpage.",
-	'ICMP' => "<strong>Internet Control Message Protocol</strong> A core Internet protocol which, when used together with TCP and UDP, is used to indicate error conditions and the like. It is also used for \\'pinging\\' hosts to check for availability.",
-	'RED' => "<strong>RED</strong> The external (Internet) connected interface and network.",
-	'ISP' => "<strong>Internet Service Provider</strong> A company or other party that provides Internet access.",
-	'Setup program' => "<strong>Setup</strong> SmoothWall\\'s commandline (console) program used for performing low-level configuration tasks such as configuring network interfaces and setting user passwords.",
-	'JavaScript' => "<strong>JavaScript</strong> A programming language built into most web browsers enabling dynamic content and realtime input validation, amongst other things.",
-	'USB' => "<strong>Universal Serial Bus</strong> A serial bus standard for interfacing devices such as cameras, printers and modems to computers.",
-	'Java' => "<strong>Java</strong> A platform-independent programming language and engine for running such programs.",
-	'IGMP' => "<strong>Internet Group Management Protocol</strong> This is a communications protocol used to manage the membership of Internet Protocol multicast groups. It should generally be ignored unless a machine is part of such a group.",
-);
+%glossary = (
 
-1;
+%baseglossary
+
+);
